07a4269f50a0d815fec3ff335964d5544c5f02e8
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-13 16:03:10 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -12,7 +12,11 @@ attr_accessor :title, :author
   end
   
   def author_name
+<<<<<<< HEAD
     if self.author
+=======
+    if self.author.name
+>>>>>>> 1bc96b1d017baa4be76aeaa24cbd19733594ca39
     author = self.author.name
     else
     author = nil
